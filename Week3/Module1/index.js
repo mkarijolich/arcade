6f69297cec10b9ea3c35bad4ed75eecbd5392ee8
fsa218 <fsa218@fsa218s-air.myfiosgateway.com>
2021-08-24 19:35:30 -0400

    Module 1 upload

---
+++ Week3/Module1/index.js
@@ -0,0 +1,22 @@
+/* 
+
+Group Questions 
+
+1. What is programming?
+2. What programming languages do you know or have heard of?
+3. How can Pseudocode be helpful?
+4. Debugging
+
+Topics
+
+- Using the Console
+- Commenting Code
+
+*/
+
+// Data Types
+
+// Variables
+
+// Errors
+
