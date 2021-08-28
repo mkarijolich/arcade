c54f88ddbcdeb14d58a2c9d515a34d846a07432f
fsa218 <fsa218@fsa218s-MacBook-Air.local>
2021-08-28 14:21:34 -0400

    debugging example added

--- Week3/Module3/3-debugging.js
+++ Week3/Module3/3-debugging.js
@@ -2,4 +2,20 @@
 
 // Group Questions
 // What is debugging?
+// troubleshooting your code when there is errors
+// making sure your code is doing what you want it to do
+// ways we have debugged: fixing syntax errors, console.log, testing
 
+function printing() {
+    // debugger // keyword used to start the debugger --> better for browser
+    for (let i = 10; i >= 1; i--) {
+        console.log("i is: ", i)
+        
+        if (i === 7) { //setting a breakpoint --> better for editor
+            break // break the loop; can be used to terminate for loop early
+        }
+
+    }
+}
+
+printing() // remember to invoke your function
